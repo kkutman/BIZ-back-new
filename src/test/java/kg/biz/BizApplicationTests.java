7d3db86a05a8dd2ad31cccc3fe0dd26ff4cfea2c
a1tbaev <aitbaevadil17@gmail.com>
2023-09-15 23:00:02 +0600

    Initial commit

---
+++ src/test/java/kg/biz/BizApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.biz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
